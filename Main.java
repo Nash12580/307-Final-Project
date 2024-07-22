288067c2d097daacbca831e7b117eeb9adb472e9
Nashali Vicente Lopez <116195984+Nash12580@users.noreply.github.com>
2024-07-22 13:54:34 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String testFilepath = "C:\\Users\\Nasha\\JavaProjects\\CSC 307\\PaintApp";
+        CodeParser.parseJavaDirectory(testFilepath);
+    }
+}
