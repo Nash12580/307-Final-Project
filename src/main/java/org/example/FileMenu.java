ce9bc71cd0a023af90187519a5c9f5f8387b0a78
Veer Bhagia <bhagia.veer@gmail.com>
2024-07-28 18:45:36 -0700

    Removed Load function (vestigal) Fixed Save
    Changed FileMenu.java

--- src/main/java/org/example/FileMenu.java
+++ src/main/java/org/example/FileMenu.java
@@ -1,8 +1,12 @@
 package org.example;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 /** @author Veer Bhagia **/
 
 public class FileMenu extends JMenuBar {
@@ -11,34 +15,40 @@ public class FileMenu extends JMenuBar {
         JMenu fileMenu = new JMenu("File");
         add(fileMenu);
 
-        JMenuItem loadMenuItem = new JMenuItem("Load");
-        loadMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                int returnValue = fileChooser.showOpenDialog(null);
-                if (returnValue == JFileChooser.APPROVE_OPTION) {
-                    String selectedPath = fileChooser.getSelectedFile().getPath();
-                    try {
-                        CodeParser.parseJavaDirectory(selectedPath);
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            }
-        });
-        fileMenu.add(loadMenuItem);
+        // Removed Load Menu Item
 
         JMenuItem saveMenuItem = new JMenuItem("Save");
         saveMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Get the current frame (Main class) to capture the content
+                JFrame mainFrame = (JFrame) SwingUtilities.getWindowAncestor(FileMenu.this);
+                if (mainFrame == null) {
+                    return; // Exit if no frame found
+                }
+
+                // Create a BufferedImage of the same size as the frame
+                Dimension size = mainFrame.getSize();
+                BufferedImage image = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
+
+                // Render the frame's content to the BufferedImage
+                Graphics2D graphics = image.createGraphics();
+                mainFrame.paint(graphics);
+                graphics.dispose();
+
+                // Save the BufferedImage to a file
                 JFileChooser fileChooser = new JFileChooser();
-                int returnValue = fileChooser.showSaveDialog(null);
-                if (returnValue == JFileChooser.APPROVE_OPTION) {
-                    String selectedPath = fileChooser.getSelectedFile().getPath();
-                    System.out.println("Save to: " + selectedPath);
+                fileChooser.setDialogTitle("Specify a file to save");
+                int userSelection = fileChooser.showSaveDialog(null);
+                if (userSelection == JFileChooser.APPROVE_OPTION) {
+                    File fileToSave = fileChooser.getSelectedFile();
+                    try {
+                        ImageIO.write(image, "png", fileToSave);
+                        JOptionPane.showMessageDialog(null, "Screenshot saved to: " + fileToSave.getAbsolutePath());
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        JOptionPane.showMessageDialog(null, "Error saving screenshot: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    }
                 }
             }
         });
