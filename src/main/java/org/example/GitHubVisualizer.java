e76850bbd4046c8f16fb76971024a3987180dbdd
Veer Bhagia <bhagia.veer@gmail.com>
2024-07-28 18:27:52 -0700

    Improved Commit Graph
    Changed GitHubVisualizer.java

--- src/main/java/org/example/GitHubVisualizer.java
+++ src/main/java/org/example/GitHubVisualizer.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
 import org.jfree.chart.ChartFactory;
@@ -6,17 +7,24 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
+
 import javax.swing.*;
 import java.awt.*;
-/** @author Veer Bhagia **/
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
 
+/** @author Veer Bhagia **/
 public class GitHubVisualizer extends JFrame {
+
     public GitHubVisualizer(String title, DefaultCategoryDataset dataset) {
         super(title);
         JFreeChart lineChart = ChartFactory.createLineChart(
                 title,
-                "Time",
-                "Commits",
+                "Date",
+                "Number of Commits",
                 dataset,
                 PlotOrientation.VERTICAL,
                 true, true, false);
@@ -28,10 +36,29 @@ public class GitHubVisualizer extends JFrame {
     public static void displayCommitHistory(String owner, String repo) throws Exception {
         JsonArray commits = GitHubAPIClient.fetchRepoCommits(owner, repo);
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        // Map to count commits per day
+        Map<Date, Integer> commitsPerDay = new TreeMap<>();
+        SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
         for (int i = 0; i < commits.size(); i++) {
             JsonObject commit = commits.get(i).getAsJsonObject();
-            String date = commit.get("commit").getAsJsonObject().get("author").getAsJsonObject().get("date").getAsString();
-            dataset.addValue(i, "Commits", date);
+            String dateStr = commit.get("commit").getAsJsonObject().get("author").getAsJsonObject().get("date").getAsString();
+            try {
+                Date date = inputFormat.parse(dateStr);
+                // Normalize the date to remove the time part
+                Date normalizedDate = new SimpleDateFormat("yyyy-MM-dd").parse(new SimpleDateFormat("yyyy-MM-dd").format(date));
+                commitsPerDay.put(normalizedDate, commitsPerDay.getOrDefault(normalizedDate, 0) + 1);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Add data to dataset
+        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd");
+        for (Map.Entry<Date, Integer> entry : commitsPerDay.entrySet()) {
+            String formattedDate = outputFormat.format(entry.getKey());
+            dataset.addValue(entry.getValue(), "Commits", formattedDate);
         }
         GitHubVisualizer chart = new GitHubVisualizer("Commit History", dataset);
         chart.pack();
